5bd2dac1a87e05488d5dd2efd9331588c5a53937
DeveloperFarrell <neitengmonteroza@gmail.com>
2025-10-05 01:05:38 +0900

    Initialize repository

---
+++ src/subscription.ts
@@ -0,0 +1,13 @@
+import AtpAgent from '@atproto/api'
+import {
+  OutputSchema as RepoEvent,
+  isCommit,
+} from './lexicon/types/com/atproto/sync/subscribeRepos'
+import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
+
+export class FirehoseSubscription extends FirehoseSubscriptionBase {
+  async handleEvent(evt: RepoEvent) {
+    if (!isCommit(evt)) return
+    const ops = await getOpsByType(evt)
+  }
+}
